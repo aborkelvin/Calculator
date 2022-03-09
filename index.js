a6099474120d43515a85815d12f4c4bfc2b5d732
Kelvin <aborkelvin@gmail.com>
2022-03-09 15:30:46 +0100

    Finished Work
    html
    Properly rearranged the buttons for a better interface
    Js
    Moved the declarations of variables up
    Created a new function which gets the value of the previous values in
        the input displays, runs an array method(filter), to check for
        previous operator signs and on seeing one,runs the solve function
        on the previous values.
    Moved the function previously called onclick of equals, to be a standing
        function as it would be needed in more than one place(checkprevious)
    Added a delete fctn which takes the value in input, converts it to array
        pops out the last value and converts back to string.
    Added a listener for keys so the input can be made with the keyboard
    css
    Colored the body and container and Properly positioned them
    Arranged the clear and delete buttons in their proper grid space

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 let sickle = document.querySelector('input');
-
+const equals = document.querySelector('.equals');
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const over = document.querySelector('.over');
+const times = document.querySelector('.times');
+const numbtn = document.querySelectorAll('.but');
+const clearbtn = document.querySelector('.clear');
+const delbtn = document.querySelector('.delbtn');
 
 function add(a,b){
     let sum = a + b;
-    //alert(sum);
     sickle.value = sum;
 
 }
 
 function subtract(a,b) {
     let diff = a - b;
-    //alert(diff);
     sickle.value = diff;
 }
 
 function multiply(a,b){
     let product = a * b;
-    //alert(product);
     sickle.value = product;
 }
 
 function divide(a,b) {
     let quot= a/b;
-    //alert(quot);
     sickle.value = quot;
 }
 
@@ -46,58 +49,71 @@ function operate(a,operator,c) {
     }
 }
 
-const equals = document.querySelector('.equals');
-
-const plus = document.querySelector('.plus');
-const minus = document.querySelector('.minus');
-const over = document.querySelector('.over');
-const times = document.querySelector('.times');
-const but = document.querySelectorAll('.but');
-const clear = document.querySelector('.clear');
 
 
-/*function funy(){ //No use
-    let val = document.querySelector('input').value;
-    let first = Number(val);
-    console.log(first);
-}*/
 
 function input(a){
     sickle.value = sickle.value + a;
     let store = sickle.value;
 }
 
-plus.addEventListener("click",function(){input('+');});
+/* Onclick of the sign(+-/*) buttons, call function checkprevious,which gets the value of the previous values 
+in the input displays, runs an array method(filter), to check for previous operator signs,
+on seeing one, it runs the solve function on the previous values, then includes the + sign
+with the result using the input function */
+function checkprevious(){
+    let hold = sickle.value;
+    let fas = hold.split('');
+    fas.filter((item)=>{
+        if(item == '+' || item == '-'|| item == '*' || item == '/'){
+            solve();
+        }
+    })
+}
 
-minus.addEventListener("click",function(){input('-');});
-times.addEventListener("click",function(){input('*');});
-over.addEventListener("click",function(){input('/');});
+plus.addEventListener("click",function(){       
+    checkprevious();
+    input('+');
+});
 
-but[0].addEventListener('click',function (){input('0');})
-but[1].addEventListener('click',function (){input('1');})
-but[2].addEventListener('click',function (){input('2');})
-but[3].addEventListener('click',function (){input('3');})
-but[4].addEventListener('click',function (){input('4');})
-but[5].addEventListener('click',function (){input('5');})
-but[6].addEventListener('click',function (){input('6');})
-but[7].addEventListener('click',function (){input('7');})
-but[8].addEventListener('click',function (){input('8');})
-but[9].addEventListener('click',function (){input('9');})
+minus.addEventListener("click",function(){
+    checkprevious();
+    input('-');
+});
+
+times.addEventListener("click",function(){
+    checkprevious();
+    input('*');});
+
+over.addEventListener("click",function(){
+    checkprevious();
+    input('/');});
+
+numbtn[0].addEventListener('click',function (){input('0');})
+numbtn[1].addEventListener('click',function (){input('1');})
+numbtn[2].addEventListener('click',function (){input('2');})
+numbtn[3].addEventListener('click',function (){input('3');})
+numbtn[4].addEventListener('click',function (){input('4');})
+numbtn[5].addEventListener('click',function (){input('5');})
+numbtn[6].addEventListener('click',function (){input('6');})
+numbtn[7].addEventListener('click',function (){input('7');})
+numbtn[8].addEventListener('click',function (){input('8');})
+numbtn[9].addEventListener('click',function (){input('9');})
+numbtn[10].addEventListener('click',function (){input('.');})
+
 
 
 /*for(var i = 0; i<10;i++){ this didn't work though,should have done the above list of codes.
-    but[i].addEventListener('click',function (){input(i.toString);})
+    numbtn[i].addEventListener('click',function (){input(i.toString);})
 }
 
-but.forEach(function(item){ This too
-    let itsarray = Array.from(but);
+numbtn.forEach(function(item){ This too
+    let itsarray = Array.from(numbtn);
     let ind = itsarray.indexOf(item);
     item.addEventListener('click',function (){input(ind.toString());})
 })
 */ 
-
-
-equals.addEventListener('click',function(){
+function solve(){
     let hold = sickle.value;
     let fas = hold.split('');
     for(let i =0;i<hold.length;i++){
@@ -126,6 +142,41 @@ equals.addEventListener('click',function(){
     }
     
     
-})
+}
 
-clear.addEventListener('click',function(){sickle.value = ''});
+equals.addEventListener('click',solve);
+
+clearbtn.addEventListener('click',function(){sickle.value = ''});
+
+//The del btn backspaces by converting to array,removing last element and converting back
+delbtn.addEventListener('click',function(){
+     let hold = sickle.value;
+     let fas = hold.split('');
+    fas.pop();
+    fas = fas.join('');
+    sickle.value = fas;
+ })
+
+/*To add event listeners for keys,so numbers and operators can be added with the keyboard*/
+document.addEventListener('keydown',function(event){
+    //listens for the numbers
+    let numbers = [0,1,2,3,4,5,6,7,8,9,'.'];
+    for(let i = 0;i<numbers.length;i++){ 
+        if(event.key == numbers[i]){
+            input(numbers[i].toString());
+        }
+    }
+    
+    //listens for the operators
+    let ops = ['+','-','*','/'];
+    for(let i = 0;i<ops.length;i++){
+        if(event.key == ops[i]){
+            checkprevious();
+            input(ops[i]);
+        }
+    }
+    if (event.key == '=') {
+        solve();
+    }
+
+})
